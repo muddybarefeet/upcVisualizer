11b49de81c497471b66373df5e70b0bd947accc0
muddybarefeet <2muddybarefeet@gmail.com>
2016-04-18 17:45:58 -0700

    style the search bar

--- server/client/js/main/MainController.js
+++ server/client/js/main/MainController.js
@@ -28,9 +28,25 @@ app.controller('MainController', ['$scope', 'Sem3', function($scope, Sem3) {
       identifier = "upc";
     }
 
+    var edits = {
+      weight: 1000000,
+      width: 10,
+      length: 10,
+      height: 10
+    };
+
 		Sem3.apiQuery(identifier, $scope.input)
     .then(function (val) {
       console.log('back this is the data', val);
+      val.forEach(function(product) {
+        //make the weight into kg and other dimensions in cm
+        for(var key in edits) {
+          if (product[key]) {
+            var data = product[key]/edits[key];
+            product[key] = data.toFixed(2);
+          }
+        }
+      });
       $scope.apiData = val;
     })
     .catch(function (err) {
